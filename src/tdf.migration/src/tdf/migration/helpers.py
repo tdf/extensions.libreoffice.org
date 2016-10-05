045c91e52f8dd55bece9e622e8411736fc064c87
Victor Fernandez de Alba <sneridagh@gmail.com>
2016-10-05 14:51:33 +0200

    ADd mailto removal

--- src/tdf.migration/src/tdf/migration/helpers.py
+++ src/tdf.migration/src/tdf/migration/helpers.py
@@ -29,3 +29,34 @@ class MagicDebug(BrowserView):
 
     def __call__(self):
         import ipdb; ipdb.set_trace()
+
+
+class GetRidOfMailtos(BrowserView):
+
+    def __call__(self):
+        alsoProvides(self.request, IDisableCSRFProtection)
+        pc = api.portal.get_tool('portal_catalog')
+        results = pc.searchResults(portal_type=[
+            'tdf.extensionuploadcenter.eupproject',
+            'tdf.extensionuploadcenter.euprelease',
+            'tdf.extensionuploadcenter.eupreleaselink',
+            'tdf.templateuploadcenter.tupproject',
+            'tdf.templateuploadcenter.tuprelease',
+            'tdf.templateuploadcenter.tupreleaselink',
+        ])
+
+        log = []
+        for item in results:
+            obj = item.getObject()
+            if 'project' in item.portal_type and \
+               obj.contactAddress is not None:
+                if 'mailto:' in obj.contactAddress:
+                    obj.contactAddress = obj.contactAddress.replace('mailto:', '')
+                    log.append(obj.id)
+            if 'release' in item.portal_type and \
+               obj.contact_address2 is not None:
+                if 'mailto:' in obj.contact_address2:
+                    obj.contact_address2 = obj.contact_address2.replace('mailto:', '')
+                    log.append(obj.id)
+
+        return log
