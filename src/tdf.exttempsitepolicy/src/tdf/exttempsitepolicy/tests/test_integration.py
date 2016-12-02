c1ce14325ce34efc0a4c60be00ed949f0d036b8a
Victor Fernandez de Alba <sneridagh@gmail.com>
2016-12-02 08:15:16 +0100

    Add more tests

--- src/tdf.exttempsitepolicy/src/tdf/exttempsitepolicy/tests/test_integration.py
+++ src/tdf.exttempsitepolicy/src/tdf/exttempsitepolicy/tests/test_integration.py
@@ -1,15 +1,24 @@
 # -*- coding: utf-8 -*-
 """Setup tests for this package."""
-from tdf.exttempsitepolicy.testing import TDF_EXTTEMPSITEPOLICY_INTEGRATION_TESTING  # noqa
 from plone import api
 from plone.registry.interfaces import IRegistry
-from zope.component import getUtility
 from Products.CMFPlone.interfaces.controlpanel import IMailSchema
+from tdf.exttempsitepolicy.testing import TDF_EXTTEMPSITEPOLICY_INTEGRATION_TESTING  # noqa
+from tdf.extensionuploadcenter.euprelease import ValidateEUpReleaseUniqueness
+from tdf.extensionuploadcenter.eupreleaselink import ValidateEUpReleaseLinkUniqueness
+from tdf.extensionuploadcenter.euprelease import IEUpRelease
+from tdf.extensionuploadcenter.eupreleaselink import IEUpReleaseLink
+from tdf.templateuploadcenter.tuprelease import ValidateTUpReleaseUniqueness
+from tdf.templateuploadcenter.tupreleaselink import ValidateTUpReleaseLinkUniqueness
+from tdf.templateuploadcenter.tuprelease import ITUpRelease
+from tdf.templateuploadcenter.tupreleaselink import ITUpReleaseLink
+from zope.component import getUtility
+from zope.interface import Invalid
 
 import unittest
 
 
-class TestSetup(unittest.TestCase):
+class TestExtensionsValidators(unittest.TestCase):
     """Test that tdf.exttempsitepolicy is properly installed."""
 
     layer = TDF_EXTTEMPSITEPOLICY_INTEGRATION_TESTING
@@ -48,10 +57,6 @@ class TestSetup(unittest.TestCase):
         self.assertTrue(result[0].release_number, u'1.0')
 
         # Trying to create other release with the same release number will trigger
-        from tdf.extensionuploadcenter.euprelease import ValidateEUpReleaseUniqueness
-        from tdf.extensionuploadcenter.euprelease import IEUpRelease
-        from zope.interface import Invalid
-
         validator = ValidateEUpReleaseUniqueness(
             release,
             self.request,
@@ -61,3 +66,85 @@ class TestSetup(unittest.TestCase):
         )
         self.assertRaises(Invalid, validator.validate, u'1.0')
         self.assertIsNone(validator.validate(u'2.0'))
+
+        releaselink = api.content.create(
+            project,
+            'tdf.extensionuploadcenter.eupreleaselink',
+            title='releaselink'
+        )
+
+        validatorlink = ValidateEUpReleaseLinkUniqueness(
+            release,
+            self.request,
+            None,
+            IEUpReleaseLink['releasenumber'],
+            None
+        )
+        self.assertRaises(Invalid, validatorlink.validate, u'1.0')
+        self.assertIsNone(validatorlink.validate(u'3.0'))
+
+
+class TestTemplateValidators(unittest.TestCase):
+    """Test that tdf.exttempsitepolicy is properly installed."""
+
+    layer = TDF_EXTTEMPSITEPOLICY_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        self.installer = api.portal.get_tool('portal_quickinstaller')
+        registry = getUtility(IRegistry)
+        mail_settings = registry.forInterface(
+            IMailSchema, prefix='plone', check=False)
+        mail_settings.smtp_host = u'localhost'
+        mail_settings.email_from_address = 'whatever'
+
+    def test_releases(self):
+        center = api.content.create(
+            self.portal,
+            'tdf.templateuploadcenter.tupcenter',
+            title='center'
+        )
+        project = api.content.create(
+            center,
+            'tdf.templateuploadcenter.tupproject',
+            title='project'
+        )
+        release = api.content.create(
+            project,
+            'tdf.templateuploadcenter.tuprelease',
+            title='release'
+        )
+
+        # Test the index
+        pc = api.portal.get_tool('portal_catalog')
+        result = pc.searchResults(portal_type="tdf.templateuploadcenter.tuprelease")
+        self.assertTrue(result[0].release_number, u'1.0')
+
+        # Trying to create other release with the same release number will trigger
+        validator = ValidateTUpReleaseUniqueness(
+            release,
+            self.request,
+            None,
+            ITUpRelease['releasenumber'],
+            None
+        )
+        self.assertRaises(Invalid, validator.validate, u'1.0')
+        self.assertIsNone(validator.validate(u'2.0'))
+
+        releaselink = api.content.create(
+            project,
+            'tdf.templateuploadcenter.tupreleaselink',
+            title='releaselink'
+        )
+
+        validatorlink = ValidateTUpReleaseLinkUniqueness(
+            release,
+            self.request,
+            None,
+            ITUpReleaseLink['releasenumber'],
+            None
+        )
+        self.assertRaises(Invalid, validatorlink.validate, u'1.0')
+        self.assertIsNone(validatorlink.validate(u'3.0'))
