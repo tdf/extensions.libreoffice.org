b33efefdcdcaee003d3b2dab9f716f5796b27391
andreasma <maand@gmx.de>
2019-04-19 11:57:52 +0200

    pep8 fixes

--- src/tdf.exttempsitepolicy/src/tdf/exttempsitepolicy/browser/frontpage.py
+++ src/tdf.exttempsitepolicy/src/tdf/exttempsitepolicy/browser/frontpage.py
@@ -5,13 +5,10 @@ from tdf.exttempsitepolicy import _
 from Acquisition import aq_inner, aq_parent
 
 
-
 class frontpageView(BrowserView):
 
     """ The view of the LibreOffice extensions and templates frontpage
     """
-
-
 
     def tupproject_count(self):
         """Return number of projects
@@ -19,9 +16,8 @@ class frontpageView(BrowserView):
         context = aq_inner(self.context)
         catalog = api.portal.get_tool(name='portal_catalog')
 
-        return len(catalog(portal_type=('tdf.templateuploadcenter.tupproject', 'tdf.templateuploadcenter.tupsmallproject')))
-
-
+        return len(catalog(portal_type=('tdf.templateuploadcenter.tupproject',
+                                        'tdf.templateuploadcenter.tupsmallproject')))
 
     def tuprelease_count(self):
         """Return number of downloadable files
@@ -31,37 +27,31 @@ class frontpageView(BrowserView):
 
         return len(catalog(portal_type='tdf.templateuploadcenter.tuprelease'))
 
-
-
     def get_newest_templateprojects(self):
         self.catalog = api.portal.get_tool(name='portal_catalog')
         sort_on = 'created'
         contentFilter = {
-                          'sort_on' : sort_on,
-                          'sort_order' : 'reverse',
+                          'sort_on': sort_on,
+                          'sort_order': 'reverse',
                           'review_state': 'published',
-                          'portal_type':('tdf.templateuploadcenter.tupproject',
-                                         'tdf.templateuploadcenter.tupsmallproject')}
+                          'portal_type': ('tdf.templateuploadcenter.tupproject',
+                                          'tdf.templateuploadcenter.tupsmallproject')}
 
         results = self.catalog(**contentFilter)
 
         return results
-
-
 
     def get_latest_template_releases(self):
-        self.catalog =api.portal.get_tool(name='portal_catalog')
+        self.catalog = api.portal.get_tool(name='portal_catalog')
         sort_on = 'created'
-        contentFilter = {'sort_on' : sort_on,
-                         'sort_order' : 'reverse',
-                         'review_state' : 'final',
-                         'portal_type' : ('tdf.templateuploadcenter.tuprelease',
-                                          'tdf.templateuploadcenter.tupreleaselink' ),
+        contentFilter = {'sort_on': sort_on,
+                         'sort_order': 'reverse',
+                         'review_state': 'final',
+                         'portal_type': ('tdf.templateuploadcenter.tuprelease',
+                                         'tdf.templateuploadcenter.tupreleaselink'),
                          }
-
         results = self.catalog(**contentFilter)
         return results
-
 
     def get_latest_temp_releases(self):
         published_projects = api.content.find(portal_type='tdf.templateuploadcenter.tupproject',
@@ -71,17 +61,13 @@ class frontpageView(BrowserView):
             project = brain.getObject()
             release_uids += [brain.UID for brain in api.content.find(context=project,
                                                                      portal_type=('tdf.templateuploadcenter.tuprelease',
-                                                                                  'tdf.templateuploadcenter.tupreleaselink' ))]
+                                                                                  'tdf.templateuploadcenter.tupreleaselink'))]
         releases = api.content.find(UID=release_uids,
                                     review_state='final',
                                     sort_on='created',
                                     sort_order='reverse')
 
         return(releases)
-
-
-
-
 
     def eupproject_count(self):
         """Return number of projects
@@ -92,8 +78,6 @@ class frontpageView(BrowserView):
         return len(catalog(portal_type=('tdf.extensionuploadcenter.eupproject',
                                         'tef.extensionuploadcenter.eupsmallproject')))
 
-
-
     def euprelease_count(self):
         """Return number of downloadable files
         """
@@ -102,22 +86,19 @@ class frontpageView(BrowserView):
 
         return len(catalog(portal_type='tdf.extensionuploadcenter.euprelease'))
 
-
-
     def get_newest_extensionprojects(self):
         self.catalog = api.portal.get_tool(name='portal_catalog')
         sort_on = 'created'
         contentFilter = {
-                          'sort_on' : sort_on,
-                          'sort_order' : 'reverse',
+                          'sort_on': sort_on,
+                          'sort_order': 'reverse',
                           'review_state': 'published',
-                          'portal_type':('tdf.extensionuploadcenter.eupproject',
-                                         'tdf.extensionuploadcenter.eupsmallproject')}
+                          'portal_type': ('tdf.extensionuploadcenter.eupproject',
+                                          'tdf.extensionuploadcenter.eupsmallproject')}
 
         results = self.catalog(**contentFilter)
 
         return results
-
 
     def get_latest_ext_releases(self):
         published_projects = api.content.find(portal_type='tdf.extensionuploadcenter.eupproject',
@@ -127,7 +108,7 @@ class frontpageView(BrowserView):
             project = brain.getObject()
             release_uids += [brain.UID for brain in api.content.find(context=project,
                                                                      portal_type=('tdf.extensionuploadcenter.euprelease',
-                                                                                  'tdf.extensionuploadcenter.eupreleaselink' ))]
+                                                                                  'tdf.extensionuploadcenter.eupreleaselink'))]
         releases = api.content.find(UID=release_uids,
                                     review_state='final',
                                     sort_on='created',
